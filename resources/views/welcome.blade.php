4b40fb7df9fe84cf85e9c7a98714ec5e1df162d9
kinyeramos2021 <kinyeramo@gmail.com>
2021-06-10 22:01:44 +0300

    adding Bootstrap to master layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,2 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-    </head>
-    <body>
-    </body>
-</html>
+@extends('layouts.master')
+<h1>Some Content</h1>
