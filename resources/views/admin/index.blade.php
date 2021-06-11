8c359cec314b3c3fc46a759eefeeef6bcd374a87
kinyeramos2021 <kinyeramo@gmail.com>
2021-06-12 02:54:08 +0300

    adding post request Router

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.admin')
 @section('content')
  <div class="row">
-   <div class="col-md-12">
-      <a href="" class=btn btn-success>New Post</a>
+    <div class="col-md-12">
+       <a href="{{ Route('admin.create')}}" class=btn btn-success>New Post</a>
     </div>
     <hr>
     <div class="row">
         <div class="col-md-12">
-           <p><strong>Learning Laravel</strong><a href="">Edit</a></p>
+           <p><strong>Learning Laravel</strong><a href="{{ Route('admin.edit',['id' => 1]) }}">Edit</a></p>
         </div>
     </div>
  </div>
