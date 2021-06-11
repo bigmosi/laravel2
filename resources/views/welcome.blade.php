3647f1946279fc733fc0c3c7f0dbce977a6f7ce1
kinyeramos2021 <kinyeramo@gmail.com>
2021-06-11 18:00:19 +0300

    Control structures

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,18 @@
 @extends('layouts.master')
 
 @section('content')
-   <div>
-   <h1>Some content</h1>
-   <p>{{"Hello"}}</p>
+   <div class="row">
+     <div class="col-md-12">
+         <h1>Control Structures</h1>
+         @if(true)
+            <p>This only display if it is true</p>
+         @else
+            <p>This only display if it is false</p>
+         @endif
+         <hr>
+         @for($i = 0;$i < 5; $i++)
+             <p>{{$i + 1}}.Iteration</p>
+         @endfor
+      </div>
    </div>
 @endsection
